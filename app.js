c6ac48af0b368e7cdda4e1480e543592749cfd39
juven0 <bocquelin@gmail.com>
2022-10-12 11:20:19 +0200

    restart return

--- app.js
+++ app.js
@@ -181,13 +181,15 @@ var listener = app.listen(process.env.PORT, function() {
 
 
 const imagedata = async ()=>{
- let ser =await GOOGLE_IMG_SCRAP({
+  await GOOGLE_IMG_SCRAP({
     search: "e-girl goth",
     query: {
       SIZE: GOOGLE_QUERY.SIZE.LARGE,
     },
 
     // excludeDomains: ["istockphoto.com", "alamy.com"]
-  })
-  return ser;
+  }).then(res => {
+    return res
+  });
+
 }
