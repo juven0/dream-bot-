a164c6130c8dc4c9d7d1d651b7af89db90bb8b2b
juven0 <bocquelin@gmail.com>
2022-10-28 16:57:56 +0200

    implementation de base de donner

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const { GOOGLE_IMG_SCRAP, GOOGLE_QUERY } = require('google-img-scrap');
 const utils = require('./fonction/utils')
 const message = require('./routs/message')
 
+//connection with data base
+require('./db')
+
 // Imports dependencies and set up http server
 const
   request = require('request'),
---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+require('dotenv').config
+
+mongoose
+    .connect(process.env.DB_URL,
+        {
+            useFindAndModify: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+    .then(()=>{
+        console.log('connected to mongodb 😎')
+    })
+    .catch((err)=>{
+        console.log("can't connect to data base")
+    })
