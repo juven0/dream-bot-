d09c9976294b4db04cab9b1e67bbd5b01bb73acb
juven0 <bocquelin@gmail.com>
2023-03-24 16:24:44 +0300

    telechargement et envoy de l'image

--- fonction/downloader.js
+++ fonction/downloader.js
@@ -1,12 +1,14 @@
 const https  = require('https')
 const fs = require('fs')
+const path = require('path')
 
 module.exports.downloader = async(url)=>{
     let resultName
     console.log('ao izi')
     await https.get(url,(res)=>{
         resultName = Date.now()+'.jpg'
-        localPath = fs.createWriteStream(__dirname+ '../public/images/'+ resultName)
+        const elementPath= path.join(__dirname, '..')
+        localPath = fs.createWriteStream(elementPath+'/public/images/'+ resultName)
         console.log(localPath)
         console.log(resultName)
         res.pipe(localPath)
